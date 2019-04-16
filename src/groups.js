c5d3a3b20782626e6eda34e609f70933faf842e1
andrea leo <andrea.leo83@gmail.com>
2019-04-16 13:02:15 +0200

    group create

--- src/groups.js
+++ src/groups.js
@@ -6,6 +6,27 @@ var Message = function(config, single, plural){
   return {
     
 
+    create: function(name, members){
+      var create = {}
+      create["group_name"] = name;
+
+      let group_members = {};
+      members.forEach(member => {
+        group_members[member] = 1;
+      });
+      create["group_members"] = group_members;
+      
+      console.log("create", create);
+     return new Promise(function(resolve, reject){
+       request.post('/' + plural+'/'+group_id, create).then(function(data){
+         console.log("data", data);
+         resolve(data)
+       }).catch(function(err){
+         reject(err)
+       })
+     })
+   },
+
     join: function(member_id, group_id){
        var join = {}
        join["member_id"] = member_id;
--- test/models/groups.js
+++ test/models/groups.js
@@ -3,6 +3,24 @@ var expect = require('chai').expect;
 
 module.exports = function(chat21){
 
+
+  it('should create a group', function(done){
+    this.timeout(20000);
+    chat21.auth.signin('andrea.leo@frontiere21.it', 123456)
+    .then(function(logindata) {
+      chat21.groups.create("test11", [logindata.localId])
+        .then(function(data){    
+          console.log("join resolve ", data);
+        expect(data).to.exist;
+        chat21.auth.logout();
+        done();    
+    })}).catch(function(err, res){
+      console.log(res);
+      chat21.auth.logout();
+      done(err);
+    });
+  });
+
   it('should join a group', function(done){
     this.timeout(20000);
     chat21.auth.signin('andrea.leo@frontiere21.it', 123456)
