421ffb19df09cbec88ed1c91ea83896bb4012d61
andrea leo <andrea.leo83@gmail.com>
2019-12-12 10:26:03 +0100

    typing

--- src/conversations.js
+++ src/conversations.js
@@ -32,6 +32,59 @@ var Conversation = function(config, single, plural){
       })
     },
 
+
+    typing: function(recipient_id, writer_id){
+            
+      var body = {};
+      if (writer_id) {
+        body["writer_id"] = writer_id;
+      }
+      console.log("body", body);
+
+      var admintoken = "";
+      if (config.admintoken) {
+       admintoken = "?token=" + config.admintoken;
+       console.log("chat21-node-sdk admintoken", admintoken);
+      }
+
+      return new Promise(function(resolve, reject){       
+
+        request.put('/typings/'+recipient_id+ admintoken, body).then(function(data){
+          console.log("chat21-node-sdk conversation typing", data);
+          resolve(data)
+        }).catch(function(err){
+          console.log("chat21-node-sdk conversation typing", err);
+          reject(err)
+        })
+      })
+    },
+
+    stopTyping: function(recipient_id, writer_id){
+            
+      var body = {};
+      if (writer_id) {
+        body["writer_id"] = writer_id;
+      }
+      console.log("body", body);
+
+      var admintoken = "";
+      if (config.admintoken) {
+       admintoken = "?token=" + config.admintoken;
+       console.log("chat21-node-sdk admintoken", admintoken);
+      }
+
+      return new Promise(function(resolve, reject){       
+
+        request.delete('/typings/'+recipient_id+ admintoken, body).then(function(data){
+          console.log("chat21-node-sdk conversation stoptypings", data);
+          resolve(data)
+        }).catch(function(err){
+          console.log("chat21-node-sdk conversation stoptypings", err);
+          reject(err)
+        })
+      })
+    },
+
    
 
   }
