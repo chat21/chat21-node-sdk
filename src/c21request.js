cfb8ca645e356cc21c602747397e277e257416bf
andrea leo <andrea.leo83@gmail.com>
2019-12-27 16:25:30 +0100

    support put

--- src/c21request.js
+++ src/c21request.js
@@ -40,6 +40,25 @@ var C21Request = function(config){
     },
 
 
+    put: function(uri, data){
+      var url = config.url + '/api/' + config.appid + uri;
+      var options = {
+        url: url,
+        headers: {
+          Authorization: config.authorization
+        },
+        json: data
+      }
+      // console.log("options", options);
+      return new Promise(function(resolve, reject){
+        request.put(options, function(err, res, body){
+          if (err) { reject(err, res); }
+          resolve(body);
+        });
+      })
+    },
+
+
     delete: function(uri, data){
       var url = config.url + '/api/' + config.appid + uri;
       var options = {
